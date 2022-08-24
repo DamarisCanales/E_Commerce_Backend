cf2d35bbbcb4702f063d36051fbd5bcd7512c639
Damaris Canales <damariscanales00@gmail.com>
2022-08-24 18:59:09 -0400

    Save

--- config/connection.js
+++ config/connection.js
@@ -15,9 +15,10 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: "localhost",
-      dialect: "mysql",
-      port: 3306,
+      host: 'localhost',
+      user: 'root',
+      password: 'rootroot',
+      database: 'employee_db'
     }
   );
 }
