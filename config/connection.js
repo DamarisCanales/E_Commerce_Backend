0951bb30d51438f7d69b6a420f5e23b5dae413ac
Damaris Canales <damariscanales00@gmail.com>
2022-08-24 18:15:43 -0400

    Save

---
+++ config/connection.js
@@ -0,0 +1,25 @@
+//sets up the secure password and username
+require("dotenv").config();
+
+//import the Sequelize constructor from the library
+const Sequelize = require("sequelize");
+
+//below following code is used after heroku has been confirmed
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
+}
+
+module.exports = sequelize;
